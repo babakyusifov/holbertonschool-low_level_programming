90e62e9e09ca3c26bd24341d4ca1e161a54d1a86
babakyusifov <babakyusifov>
2025-02-25 23:07:27 +0400

    sizes variables

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,13 +9,13 @@ int main(void)
 int intType;
 float floatType;
 long int longIntType;
-long long int longLongIntType;
+long long int longlongIntType;
 char charType;
 
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of long int: %zu bytes\n", sizeof(longType));
-printf("Size of long long int: %zu bytes\n", sizeof(longLongType));
+printf("Size of long int: %zu bytes\n", sizeof(longintType));
+printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
 printf("Size of char: %zu byte\n", sizeof(charType));
 
 return (0);
