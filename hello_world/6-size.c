2f3608d96366426c0e7188774168930eacb42bef
babakyusifov <babakyusifov>
2025-02-25 23:20:20 +0400

    my brain fired

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
-*Main - printf the size
-*Return : Always 0 (Success)
+* main - printf the size of various data types
+*
+* Return : Always 0 (Success)
 */
+
 int main(void)
 {
 int intType;
@@ -12,11 +14,11 @@ long int longintType;
 long long int longlongintType;
 char charType;
 
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of long int: %zu bytes\n", sizeof(longintType));
-printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
 
 return (0);
 }
