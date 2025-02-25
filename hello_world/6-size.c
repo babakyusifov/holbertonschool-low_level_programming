eae946107c9d98bb0ac442f3d2a0aa8b4a79a8a1
babakyusifov <babakyusifov>
2025-02-25 23:26:33 +0400

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - printf the size of various data types
-*
-* Return : Always 0 (Success)
-*/
+ * main - size oversize files of variables
+ *
+ * Return always 0 (Success)
+ */
 
 int main(void)
 {
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
-printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
 return (0);
 }
