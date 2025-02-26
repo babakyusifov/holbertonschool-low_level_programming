e9e2e85a223440da5b515e5d7e819593c7433b1a
babakyusifov <babek@div.edu.az>
2025-02-26 21:00:02 +0400

    0-positive_or_negative.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive \n", n);
+printf("is positive\n", n);
 }
 else if (n < 0)
 {
-printf("is negative \n", n);
+printf("is negative\n", n);
 }
 else
 {
-printf("is zero. \n");
+printf("is zero.\n");
 }
 return (0);
 }
