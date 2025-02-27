6e91e0d88e66869e6aafa05b827085de255bd1cb
babakyusifov <babek@div.edu.az>
2025-02-27 23:16:16 +0400

    Generates a random number and checks the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ n = rand() - RAND_MAX / 2;
 int x = n % 10;
 if (x > 5)
 {
-printf("Last digit of %d and %d is greater than 5\n", n, x);
+printf("Last digit of %d is %d and greater than 5\n", n, x);
 }
 else if (x == 0)
 {
-printf("Last digit of %d and %d is 0\n", n, x);
+printf("Last digit of %d is %d and 0\n", n, x);
 }
 else
 {
-printf("Last digit of %d and %d is less than 6 and not 0\n", n, x);
+printf("Last digit of %d is %d and less than 6 and not 0\n", n, x);
 }
 return (0);
 }
