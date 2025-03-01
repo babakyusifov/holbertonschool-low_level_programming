db4309c1b3563865f5bda5ef5c53d37a222f59eb
babakyusifov <babek@div.edu.az>
2025-03-01 17:54:56 +0400

    prints all single digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int i = 48;
+for (; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
