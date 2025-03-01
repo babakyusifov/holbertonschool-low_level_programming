a5de1c3f5afc1f8f31d347aa4303e6229553289f
babakyusifov <babek@div.edu.az>
2025-03-01 16:51:42 +0400

    prints the alphabet in lowercase,uupercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+char kh = 'a';
+char bh = 'A';
+for (; kh <= 'z' ; kh++)
+for (; bh <= 'Z' ; bh++)
+{
+putchar(kh);
+putchar(bh);
+}
+putchar('\n');
+return (0);
+}
