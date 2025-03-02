d363990ee96145298f79efb02ba0a5646e6a2dad
babakyusifov <babek@div.edu.az>
2025-03-02 16:38:21 +0400

    Prototype: void print_alphabet(void);

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Prints the alphabet in lowercase followed by a new line.
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line.
  * Return: 0
  */
 void print_alphabet(void)
