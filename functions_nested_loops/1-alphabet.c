da7aae1555b12ebb3ce35db5c641ff64bde80569
babakyusifov <babek@div.edu.az>
2025-03-02 16:49:23 +0400

    Prototype: void print_alphabet(void);

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *main - Entry point of the program.
- *Return: 0
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -12,8 +13,9 @@ return (0);
 
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line.
- *
  * Description: This function iterates through the lowercase alphabet
+ * from 'a' to 'z' and prints each character using _putchar. After
+ * printing all the letters, it prints a newline character.
  */
 void print_alphabet(void)
 {
