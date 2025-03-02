61bd293f079ca050b5936a78c936a4b6c6d57efc
babakyusifov <babek@div.edu.az>
2025-03-02 16:54:21 +0400

    Prototype: void print_alphabet(void);

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -9,13 +9,12 @@
  */
 void print_alphabet(void)
 {
-    char c;
-
-    c = 'a';
-    while (c <= 'z')
-    {
-        _putchar(c);
-        c++;
-    }
-    _putchar('\n');
+char c;
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
 }
