2b1648da6208505e18a278b5018cd3b3036baee9
babakyusifov <babek@div.edu.az>
2025-03-02 15:12:06 +0400

    Prints numbers of base 16 in lowercase

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+  * main - Prints numbers of base 16 in lowercase
+  * Return: Always 0 success
+*/
+int main(void)
+{
+int num = 0;
+for (; num < 10; num++)
+{
+putchar(num + '0');
+if (num < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
