c5b30384e822f54478f9dc605c3e2c8cf1090689
babakyusifov <babek@div.edu.az>
2025-03-05 21:06:14 +0400

    Write a function that multiplies two integers

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * mul - that multiplies two integers..
+ * mul - Multiplies two integers..
  * @a: The first integer.
  * @b: The second integer.
  * The result of multiplying a and b.
+ * Return: The result of multiplying a and b.
  */
 int mul(int a, int b)
 {
