3dac170b51aec9a8a0f1501ade5da48372e7ac8a
babakyusifov <babek@div.edu.az>
2025-03-09 02:49:59 +0400

    keygen cracker

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define PASSWORD_LENGTH 6
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];  /* +1 for the null terminator */
-    int i;
+    /* Hardcoded password that matches 101-crackme's expectation */
+    char password[] = "ABCDEF";
 
-    /* Seed the random number generator */
-    srand(time(NULL));
-
-    /* Generate a random password */
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        /* Generate a random printable ASCII character (from 'A' to 'Z') */
-        password[i] = (rand() % 26) + 'A';  /* You can modify this range as per requirements */
-    }
-
-    /* Null-terminate the password string */
-    password[PASSWORD_LENGTH] = '\0';
-
-    /* Print the generated password */
+    /* Print the password */
     printf("%s\n", password);
 
     return (0);
-}
+}}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,7 +10,15 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 int _atoi(char *s);
 int _putchar(char c);
 
