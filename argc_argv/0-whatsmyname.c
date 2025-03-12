6eff3cf59d33ce7929d598894a9a3a3f6380609a
babakyusifov <babek@div.edu.az>
2025-03-12 20:14:32 +0400

    argc_argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
  *main - a program that prints its name, followed by a new line.
  *@argv:is an array of strings containing the command-line arguments.
  *@argc:is the number of command-line arguments passed to the program.
- * Return: Always o success
+ * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
 	int i = 0;
+	(void)argc;
 while (argv[0][i] != '\0')
 	{
 	_putchar(argv[0][i]);
