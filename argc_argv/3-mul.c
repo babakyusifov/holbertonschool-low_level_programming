eb5211950600e9fd6be857a90c1beffe4fd06323
babakyusifov <babek@div.edu.az>
2025-03-12 20:49:40 +0400

    Your program should print the result of the multiplication, followed by a new line

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,28 +2,32 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - Multiplies two numbers passed as arguments.
+ * @argc: The number of arguments.
+ * @argv: An array of argument strings.
  *
- * @argc: incoming argument
- * @argv: incoming argument
- *
- * Return: Always 0 (Success)
+ * Return: 0 on success, 1 on error.
  */
-int main(int argc, char **argv)
-	{
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
 
-	 int total;
+    /* Check if exactly two arguments are passed */
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+    /* Convert arguments to integers */
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
 
-	total = (int) *argv[1] * (int) *argv[2];
+    /* Multiply the numbers */
+    result = num1 * num2;
 
-	printf("%d\n", total);
+    /* Print the result */
+    printf("%d\n", result);
 
-	return (0);
-
-	}
+    return (0);
+}
