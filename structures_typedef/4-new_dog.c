846c34727a72ffada693405deb11699ac7052397
babakyusifov <babek@div.edu.az>
2025-03-21 16:12:26 +0400

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,24 +8,24 @@
  */
 char *_strdup(char *str)
 {
-    char *dup;
-    size_t len = 0, i;
+	char *dup;
+	size_t len = 0, i;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    /* Calculate string length */
-    while (str[len])
-        len++;
+	/* Calculate string length */
+	while (str[len])
+		len++;
 
-    /* Allocate memory for duplicate */
-    dup = malloc((len + 1) * sizeof(char));
-    if (dup == NULL)
-        return (NULL);
+	/* Allocate memory for duplicate */
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
 
-    /* Copy characters */
-    for (i = 0; i <= len; i++)
-        dup[i] = str[i];
+	/* Copy characters */
+	for (i = 0; i <= len; i++)
+		dup[i] = str[i];
 
-    return (dup);
+	return (dup);
 }
