de756676d6859a5059120bbce67d7fbbdd43d8fd
babakyusifov <babek@div.edu.az>
2025-03-21 16:16:57 +0400

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,22 +2,34 @@
 #include <stdlib.h>
 
 /**
- * _strdup - creates a duplicate of a string
- * @str: the string to duplicate
- * Return: pointer to the duplicated string or NULL if it fails
+ * new_dog - entry point
+ * @name: string from main, name of pet
+ * @age: number from main, age of pet
+ * @owner: string from main, owner of pet
+ * Return: p
  */
-char *_strdup(char *str)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-char *dup;
-size_t len = 0, i;
-if (str == NULL)
-return (NULL);
-while (str[len])
-len++;
-dup = malloc((len + 1) * sizeof(char));
-if (dup == NULL)
-return (NULL);
-for (i = 0; i <= len; i++)
-dup[i] = str[i];
-return (dup);
+	dog_t *p;
+	/* reserving memory to struct*/
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	/* Cpunting name pointer*/
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
